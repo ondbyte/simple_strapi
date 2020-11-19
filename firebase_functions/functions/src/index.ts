92a07c07ace966c882febf4663aeec8c15c8a805
yadunandan <yadunandan@cws.coorgle.com>
2020-11-19 17:53:41 +0530

    booking , functions

---
+++ firebase_functions/functions/src/index.ts
@@ -0,0 +1,137 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { DocumentReference } from '@firebase/firestore-types';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+
+admin.initializeApp();
+
+
+
+var bappFCMMessageType = {
+    staffAuthorizationAsk: "staffAuthorizationAsk",
+    staffAuthorizationAskAcknowledge: "staffAuthorizationAskAcknowledge",
+    staffAuthorizationAskDeny: "staffAuthorizationAskDeny",
+    reminder: "reminder"
+};
+
+exports.sendBappMessage = functions.https.onCall(async (j, context) => {
+    "use-strict";
+    var resultString = "";
+    try {
+        if (j.type === bappFCMMessageType.staffAuthorizationAsk) {
+            resultString = await ask(j);
+        } else if (j.type === bappFCMMessageType.staffAuthorizationAskAcknowledge || j.type === bappFCMMessageType.staffAuthorizationAskDeny) {
+            resultString = await denyOrAcknowledge(j);
+        } else if (j.type === bappFCMMessageType.reminder) {
+            ///implement a reminder
+            resultString = "notImplemented";
+        }
+    } catch (e) {
+        console.log(Date.now());
+        console.log(e);
+        resultString = e;
+    }
+    return { result: resultString };
+});
+
+exports.remindForBooking = functions.https.onRequest(async (req, resp) => {
+    var reference = new DocumentReference(req.query.reference);
+
+
+});
+
+
+async function sendMessage(j:any){
+    var resultString = "fail";
+    const snaps = await admin.firestore().collection('users').where("contactNumber", "==", j.to).get();
+
+    if(snaps.size == 1){
+        var doc = snaps.docs[0];
+        var docData = doc.data();
+
+        const notification = {
+            "notification": {
+                "body": j.body,
+                "title": j.title,
+                "click_action": j.click_action
+            },
+            "data": j,
+        }
+        var resp = await admin.messaging().sendToDevice(docData.fcmToken, j);
+        if(resp.failureCount==1){
+            resultString = "fail";
+        }
+        resultString = "success";
+    }
+    return resultString;
+}
+
+
+async function denyOrAcknowledge(j:any) {
+    var resultString = "";
+    const snaps = await admin.firestore().collection('users').where("contactNumber", "==", j.to).get();
+
+    if (snaps.size === 1) {
+        var doc = snaps.docs[0];
+        var docData = doc.data();
+
+        j.uid = doc.id;
+        const staffingAuth = {
+            "notification": {
+                "body": j.body,
+                "title": j.title,
+                "click_action": j.click_action
+            },
+            "data": j,
+        }
+        admin.messaging().sendToDevice(docData.fcmToken, staffingAuth);
+    }
+    resultString = "success";
+    return resultString;
+}
+
+async function ask(j:any) {
+    var resultString = "";
+    const snaps = await admin.firestore().collection('users').where("contactNumber", "==", j.to).get();
+
+    if (snaps.size === 1) {
+        resultString = "success";
+    } else if (snaps.size === 0) {
+        resultString = "noUser";
+    } else if (snaps.size > 1) {
+        ///this should never be the case
+        resultString = "multiUser";
+    }
+
+    if (snaps.size === 1) {
+        var doc = snaps.docs[0];
+        var docData = doc.data();
+
+        j.uid = doc.id;
+        const staffingRequest = {
+            "notification": {
+                "body": j.body,
+                "title": j.title,
+                "click_action": j.click_action
+            },
+            "data": j,
+        }
+        admin.messaging().sendToDevice(docData.fcmToken, staffingRequest);
+    }
+    return resultString;
+}
+
+
+
+/* enum BusinessStaffBookedState{
+    no,
+    partially,
+    almost,
+    fully,
+  } */
