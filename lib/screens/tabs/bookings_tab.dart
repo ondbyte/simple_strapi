553195f5634f407a216f75a1cf599a958ba2e928
Pretheesh Thomas <me@bigmints.com>
2020-12-01 11:39:25 +0400

    no message

--- lib/screens/tabs/bookings_tab.dart
+++ lib/screens/tabs/bookings_tab.dart
@@ -127,8 +127,8 @@ class _BookingsTabState extends State<BookingsTab> {
   Widget _getCalender() {
     return SliverAppBar(
       elevation: 0,
-      collapsedHeight: _calenderHeight+6 ?? 132,
-      expandedHeight: _calenderHeight+6 ?? 132,
+      collapsedHeight: 140,
+      expandedHeight: 140,
       pinned: true,
       automaticallyImplyLeading: false,
       actions: [
